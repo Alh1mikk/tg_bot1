e3dea28ee66e3874c41108055884ed7c2df31137
Alh1mikk <i.pi4uev@yandex.ru>
2025-05-31 18:21:58 +0900

    надеюсь загрузит

--- bot.py
+++ bot.py
@@ -7,6 +7,11 @@ bot = telebot.TeleBot(token)
 def start(message):
     bot.reply_to(message, "Привет! Я бот для управления чатом.")
 
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
 @bot.message_handler(commands=['ban'])
 def ban_user(message):
     if message.reply_to_message: #проверка на то, что эта команда была вызвана в ответ на сообщение 
@@ -22,5 +27,32 @@ def ban_user(message):
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+@bot.message_handler(commands=['mute'])
+def mute_user(message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно замутить администратора.")
+        else:
+            bot.restrict_chat_member(chat_id, user_id, until_date=datetime.datetime.now() + datetime.timedelta(minutes=30))
+            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был замучен на 30 минут.")
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите замутить.")
+
+@bot.message_handler(commands=['unmute'])
+def unmute_user(message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно размутить администратора.")
+        else:
+            bot.restrict_chat_member(chat_id, user_id, can_send_messages=True)
+            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был размучен.")
+    else:
+        bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите размутить.")
 
 bot.infinity_polling(none_stop=True)
--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-token = "" # токен для бота
+token = "7585403678:AAHcnoPFgT2O583qXA0XLRHfi7_NCoSK0ek" # токен для бота
